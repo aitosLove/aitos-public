c1ec4725a10bc0ffb9083c34f90731513f2f6e30
luokerenx4 <luokerenx4@163.com>
2025-03-06 22:51:07 +0800

    sidebar

--- front/app/app-sidebar.tsx
+++ front/app/app-sidebar.tsx
@@ -1,4 +1,12 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import {
+  Calendar,
+  Home,
+  Inbox,
+  Search,
+  Settings,
+  Bot,
+  MessageCircle,
+} from "lucide-react";
 
 import {
   Sidebar,
@@ -10,59 +18,101 @@ import {
   SidebarMenuButton,
   SidebarHeader,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
+  SidebarMenuSubButton,
 } from "@/components/ui/sidebar";
 import Image from "next/image";
 import { ModeToggle as ThemeButton } from "./theme-button";
 
-// Menu items.
-const items = [
-  {
-    title: "Agent",
-    url: "/agent",
-    icon: Home,
-  },
-  {
-    title: "Portfolio",
-    url: "/portfolio",
-    icon: Inbox,
-  },
-  {
-    title: "Chat",
-    url: "/chat",
-    icon: Calendar,
-  },
-];
+// // Menu items.
+// const items = [
+//   {
+//     title: "Agent",
+//     url: "/agent",
+//     icon: Home,
+//   },
+//   {
+//     title: "Portfolio",
+//     url: "/portfolio",
+//     icon: Inbox,
+//   },
+//   {
+//     title: "Chat",
+//     url: "/chat",
+//     icon: Calendar,
+//   },
+// ];
 
 export function AppSidebar() {
   return (
     <Sidebar collapsible="icon">
       <SidebarContent>
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <div className="flex items-center space-x-2">
+                <Image src="/suikai.png" alt="S" width={32} height={32} />
+              </div>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <ThemeButton />
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
         <SidebarGroup>
-          <SidebarHeader>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <div className="flex items-center space-x-2">
-                  <Image src="/suikai.png" alt="S" width={32} height={32} />
-                </div>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <ThemeButton />
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarHeader>
-          <SidebarGroupLabel>Route</SidebarGroupLabel>
+          {/* <SidebarGroupLabel>Route</SidebarGroupLabel> */}
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              {/* Agent */}
+              <SidebarMenuItem key={"agent"}>
+                <SidebarMenuButton asChild>
+                  <a href={`/agent`}>
+                    <Bot />
+                    <span>{`Agent`}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+
+              {/* Chat */}
+              <SidebarMenuItem key={"chat"}>
+                <SidebarMenuButton asChild>
+                  <a href={`/chat`}>
+                    <MessageCircle />
+                    <span>{`Chat`}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              {/* Modules */}
+              <SidebarMenuItem key={"Modules"}>
+                <SidebarMenuButton asChild>
+                  <div>
+                    <Inbox />
+                    <span>{`Modules`}</span>
+                  </div>
+                </SidebarMenuButton>
+                <SidebarMenuSub>
+                  {/* Portfolio */}
+                  <SidebarMenuSubItem>
+                    <SidebarMenuSubButton asChild>
+                      <a href={`/portfolio`}>
+                        <Settings />
+                        <span>{`AI Trading`}</span>
+                      </a>
+                    </SidebarMenuSubButton>
+                  </SidebarMenuSubItem>
+
+                  {/* Defi */}
+                  <SidebarMenuSubItem>
+                    <SidebarMenuSubButton asChild aria-disabled>
+                      <div>
+                        <Settings />
+                        <span>{`Defi (Coming Soon)`}</span>
+                      </div>
+                    </SidebarMenuSubButton>
+                  </SidebarMenuSubItem>
+                </SidebarMenuSub>
+              </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
