1e062b642f8281e03d9f4368ec844a29b513f69f
luokerenx4 <luokerenx4@163.com>
2025-03-07 01:23:01 +0800

    fix

--- front/app/chat/(chat)/components/header.tsx
+++ front/app/chat/(chat)/components/header.tsx
@@ -6,7 +6,7 @@ import { IconLogo } from "@/components//ui/icons";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 
-export async function Header() {
+export default async function Header() {
   return (
     <header className="w-full fixed w-2xl p-2 flex justify-right items-center z-10 backdrop-blur lg:backdrop-blur-none bg-background/80 lg:bg-transparent">
       {/* <ModeToggle /> */}
