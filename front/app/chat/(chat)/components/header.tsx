3771ceeda23276590c87c5501f1ad2a5dfbd5399
luokerenx4 <luokerenx4@163.com>
2025-03-07 01:58:21 +0800

    fix

--- front/app/chat/(chat)/components/header.tsx
+++ front/app/chat/(chat)/components/header.tsx
@@ -1,12 +1,12 @@
-import { cn } from "@/app/chat/(chat)/lib/utils";
+// import { cn } from "@/app/chat/(chat)/lib/utils";
 import React from "react";
-import HistoryContainer from "./history-container";
-import { ModeToggle } from "./mode-toggle";
-import { IconLogo } from "@/components//ui/icons";
+import { HistoryContainer } from "./history-container";
+// import { ModeToggle } from "./mode-toggle";
+// import { IconLogo } from "@/components//ui/icons";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 
-export default async function Header() {
+export default function Header() {
   return (
     <header className="w-full fixed w-2xl p-2 flex justify-right items-center z-10 backdrop-blur lg:backdrop-blur-none bg-background/80 lg:bg-transparent">
       {/* <ModeToggle /> */}
--- front/app/chat/(chat)/components/history-container.tsx
+++ front/app/chat/(chat)/components/history-container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { History } from "./history";
 import { HistoryList } from "./history-list";
 
-export default async function HistoryContainer() {
+export function HistoryContainer() {
   const enableSaveChatHistory = process.env.ENABLE_SAVE_CHAT_HISTORY === "true";
   if (!enableSaveChatHistory) {
     return <></>;
