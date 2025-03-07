1dae90ea28ce2ceec8c684071e76151a31c2be3c
luokerenx4 <luokerenx4@163.com>
2025-03-07 18:47:55 +0800

    fix-button

--- front/app/chat/(chat)/layout.tsx
+++ front/app/chat/(chat)/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-screen w-full max-w-3xl relative">
+    <div className="min-h-screen w-full relative">
       <Header />
       {children}
       <Toaster />
