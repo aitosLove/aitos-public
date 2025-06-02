f6ebb142efd5a9a8b1ec3891683a9d6ba4eb33b6
ryu <luokerenx4@163.com>
2025-06-02 13:05:46 +0800

    remove pageArchive from tracking

--- front/pageArchive/chat/(chat)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Chat } from "./components/chat";
-import { getModels } from "@/app/chat/(chat)/lib/config/models";
-import { generateId } from "ai";
-
-export default async function Page() {
-  const id = generateId();
-  const models = await getModels();
-  return <Chat id={id} models={models} />;
-}
